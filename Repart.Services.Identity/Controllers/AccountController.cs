3bc09728460be38367c700c3ee20499e704c2057
dominique09 <dominique.septembre@gmail.com>
2018-07-27 23:22:56 -0400

    Ajout d'autorisation

--- Repart.Services.Identity/Controllers/AccountController.cs
+++ Repart.Services.Identity/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Repart.Common.Commands.Identity;
 using Repart.Services.Identity.Domain.Repositories;
@@ -22,20 +25,32 @@ namespace Repart.Services.Identity.Controllers
         }
 
         [HttpPost("register")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "SUPER_ADMIN,CREER_UTILISATEUR")]
         public async Task<IActionResult> Register([FromBody] RegisterUser command)
         => Json(await _userService.RegisterAsync(command.Email, command.Password, command.Name, command.Roles));
         
         [HttpPost("login")]
+        [AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] AuthenticateUser command)
             => Json(await _userService.LoginAsync(command.Email, command.Password));
 
         [HttpGet("users")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "SUPER_ADMIN,CREER_UTILISATEUR,GESTION_UTILISATEUR,CONSULTATION_UTILISATEUR")]
         public async Task<IActionResult> GetAllUsers()
             => Json(await _userService.GetAll());
 
         [HttpGet("user/{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> GetUser(Guid id)
-            => Json(await _userService.GetAsync(id));
+        {
+            var roles = "SUPER_ADMIN,CREER_UTILISATEUR,GESTION_UTILISATEUR,CONSULTATION_UTILISATEUR";
+            if (!roles.Split(',').Any(r => User.IsInRole(r)) && User.Identity.Name != id.ToString())
+                return Unauthorized();
+
+
+
+            return Json(await _userService.GetAsync(id));
+        }
         
         [HttpGet("user/toggle-active/{id}")]
         public async Task<IActionResult> ToggleActive(Guid id)
@@ -52,5 +67,13 @@ namespace Repart.Services.Identity.Controllers
         [HttpPost("remove-role")]
         public async Task<IActionResult> RemoveRole([FromBody] ModifyUserRole command)
             => Json(await _userService.RemoveFromRole(command.UserId, command.RoleId));
+
+        [HttpGet("Test")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult test()
+        {
+            return Json(User.Identity.Name);
+        }
+
     }
 }
--- Repart.Services.Identity/Services/CustomMongoSeeder.cs
+++ Repart.Services.Identity/Services/CustomMongoSeeder.cs
@@ -23,7 +23,7 @@ namespace Repart.Services.Identity.Services
             var roles = new List<string>
             {
                 "SUPER_ADMIN",
-                "GESTION_UTILISATEUR", "CONSULTATION_UTILISATEUR",
+                "GESTION_UTILISATEUR", "CREER_UTILISATEUR", "CONSULTATION_UTILISATEUR",
                 "GESTION_INTERVENANT", "CONSULTATION_INTERVENANT",
                 "GESTION_FORMATION", "CONSULTATION_FORMATION",
             };
