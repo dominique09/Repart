20e3ab53dbd1e34ad31f161be786fcbb1f348e51
dominique09 <dominique.septembre@gmail.com>
2018-07-30 21:04:07 -0400

    Intervenant model

--- Repart.Services.Intervenants/Domain/Models/Intervenant.cs
+++ Repart.Services.Intervenants/Domain/Models/Intervenant.cs
@@ -1,6 +1,33 @@
-﻿namespace Repart.Services.Intervenants.Domain.Models
+﻿using System;
+using Repart.Common.Exceptions;
+
+namespace Repart.Services.Intervenants.Domain.Models
 {
     public class Intervenant
     {
+        public Guid Id { get; protected set; }
+        public string Firstname { get; protected set; }
+        public string Lastname { get; protected set; }
+        public string Initiales { get; protected set; }
+        public Guid FormationId { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+
+        public Intervenant(string firstname, string lastname, string initiales, Guid formationId)
+        {
+            if(string.IsNullOrEmpty(firstname))
+                throw new RepartException("intervenant_firstname_empty",
+                    "Le prénom de l'intervenant est vide.");
+
+            if(string.IsNullOrEmpty(lastname))
+                throw new RepartException("intervenant_lastname_empty",
+                    "Le nom de l'intervenant est vide.");
+
+            Id = Guid.NewGuid();
+            Firstname = firstname;
+            Lastname = lastname;
+            Initiales = initiales;
+            FormationId = formationId;
+            CreatedAt = DateTime.UtcNow;
+        }
     }
 }
